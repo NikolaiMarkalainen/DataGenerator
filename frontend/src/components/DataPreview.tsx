eb8513eda9b0f7dcff46a3e718237510a1f7436e
NikolaiMarkalainen <nikolai.markalainen@gmail.com>
2024-10-23 13:06:53 +0300

    Added first version of preview view of object

--- frontend/src/components/DataPreview.tsx
+++ frontend/src/components/DataPreview.tsx
@@ -1,13 +1,12 @@
-import { PrimaryButton, Stack } from "@fluentui/react";
+import { PrimaryButton, DefaultButton, Stack, Text } from "@fluentui/react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
-
-
+import { variableNameForNumber } from "../types/variableEnum";
 
 
 export const DataPreview = () => {
-    const variables = useSelector((state: RootState) => state.variables);
+    const variables = useSelector((state: RootState) => state.variables.variables);
     console.log(variables);
     const navigate = useNavigate();
 
@@ -15,10 +14,40 @@ export const DataPreview = () => {
         navigate('/');
     };
 
-    return(
-        <Stack>
-            asdasdas
-            <PrimaryButton onClick={navigateBack}/>
+    const generateData = () => {
+        console.log("generate data")
+    }
+    return (
+        <Stack horizontal horizontalAlign="center" style={{minHeight: "50%"}}>
+            <Stack tokens={{childrenGap: 16}} style={{width: "50%"}}>
+                {variables && variables.length > 0 ? (
+                    variables.map((m, index) => {
+                        const typeToName = variableNameForNumber[m.type as keyof typeof variableNameForNumber];
+                        return (
+                            <Stack
+                                key={index} 
+                                enableScopedSelectors
+                                horizontal
+                                tokens={{ childrenGap: 16, padding: 22 }}
+                                styles={{root: {justifyContent: "space-around", backgroundColor:'#FFAE42'}}}
+                            >
+                                <Stack.Item>
+                                    <Text style={{color: "grey"}}>"{m.name}"</Text>
+                                </Stack.Item>
+                                    -
+                                <Stack.Item>
+                                    <Text>{typeToName}</Text>
+                                </Stack.Item>
+                            </Stack>
+                        );
+                    })
+                ) : (
+                    <Text>No variables available</Text>
+                )}
+                <PrimaryButton onClick={generateData}>Genereate Data</PrimaryButton>
+                <DefaultButton onClick={navigateBack}>Go Back</DefaultButton>
+            </Stack>
         </Stack>
-    )
+    );
+
 };
--- frontend/src/types/variableEnum.ts
+++ frontend/src/types/variableEnum.ts
@@ -9,3 +9,14 @@ export enum variableMenuItems {
     RANDOM_CUSTOM_OBJECT = 6, 
     RANDOM_ID = 7,
 }
+
+export enum variableNameForNumber {
+    "INT" = 0,
+    "STRING OPEN" = 1,
+    "STRING FIXED" = 2,
+    "STRING FNAME" = 3,
+    "STRING LNAME" = 4,
+    "STRING COUNTRY" = 5,
+    "STRING OBJECT" = 6,
+    "STRING ID" = 7,
+};
