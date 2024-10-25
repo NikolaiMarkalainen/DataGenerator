5fa99baf9251c35ff430490c1c678ab40632ef87
NikolaiMarkalainen <nikolai.markalainen@gmail.com>
2024-10-25 15:33:21 +0300

    Docker configurations to just run database and adjustment to urls

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,11 +1,10 @@
 using dotenv.net;
 using Npgsql;
-
+using Microsoft.Extensions.Logging;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 IConfiguration configuration = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json")
@@ -13,16 +12,17 @@ IConfiguration configuration = new ConfigurationBuilder()
     .Build();
 
 var app = builder.Build();
-
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+builder.Logging.AddConsole();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+logger.LogInformation($"lasdadssol: śadas");
 
 app.UseHttpsRedirection();
-
+logger.LogInformation($"lasdadssol: śadas");
 // Load environment variables and request a connection to PSQL DB
 DotEnv.Load();
 var envVars = DotEnv.Read();
@@ -35,6 +35,6 @@ try{
 };
 
 
-app.MapGet("/", () => "Tessssdsdsssdsdsdsdsdsdasdaasdasdasdasdsddsdsdsdssdst");
+app.MapGet("/", () => "Tesssasdassddsdssdsssdsdsdsdsdsdasdaasdasdasdasdsddsdsdsdssdst");
 
 app.Run();
